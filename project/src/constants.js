041e342efd2552909293e81630d7a41f61fa2bbf
Ajitharuth <ajitha.samuel@datasirpi.com>
2025-12-09 12:47:59 +0530

    added new project

---
+++ project/src/constants.js
@@ -0,0 +1,24 @@
+export const routesPath = {
+    home: '/',
+    about: '/about',
+    career: '/career',
+    platformEngineering: '/platform-engineering',
+    applicationEngineering: '/application-engineering',
+    salesforceCrm: '/salesforce-crm',
+    cyberSecurity: '/cyber-security',
+    blogs: '/blogs',
+    blogById: '/blogs/:id',
+    privacy: '/privacy',
+    terms: '/terms'
+}
+
+export const elementIds = {
+    home: "ds-home",
+    about: "ds-about",
+    services: "ds-services",
+    contact: "ds-contact",
+    career: "ds-career",
+    blogs: "ds-blogs",
+  }
+
+export const blogsApiUrl = "https://workdesk.datasirpi.com/read-blogs";
