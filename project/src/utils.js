b0d4064017b0ea0532bf1a90d9cb0c88952250be
prabudatasirpi <prabuk@datasirpi.com>
2025-12-11 18:37:53 +0530

    removed old code

--- project/src/utils.js
+++
@@ -1,6 +0,0 @@
-export const getAppUrl = () => {
-    const domain = window?.location?.origin;
-    const protocol = window?.location?.protocol;
-    const appDomain = domain?.replace("http://", "")?.replace("https://", "");
-    return `${protocol}//${appDomain}`;
-}
